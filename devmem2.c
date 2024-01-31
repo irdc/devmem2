03390965dd6a69dcf0d143b22e4d94cbd429b04d
Willemijn Coene <willemijn@irdc.nl>
2024-01-31 15:21:13 +0000

    Gc includes.

--- devmem2.c
+++ devmem2.c
@@ -29,17 +29,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <errno.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <termios.h>
-#include <sys/types.h>
-#include <sys/mman.h>
+#include <unistd.h>
 #include <err.h>
 #include <inttypes.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 int
 main(int argc, char **argv)
